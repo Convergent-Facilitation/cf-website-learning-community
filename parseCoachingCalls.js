39921e138452fc4d680f2facaa7766291ceac53b
f <f@email>
2023-10-15 01:59:55 +0200

    added coaching call for 2021-2023; added parsing of tags

--- parseCoachingCalls.js
+++ parseCoachingCalls.js
@@ -1,56 +1,89 @@
-const matter = require('gray-matter');
-const fs = require('fs-extra');
-const glob = require("glob");
-const path = require("path");
+const matter = require('gray-matter')
+const fs = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
 
 // Get all mdx files that don't start with _
-glob("resources/recordings-coaching-calls/[!_]*.mdx", function (err, files) {
-  if(err) {
-    console.log("Error finding files: " + err)
+glob('resources/recordings-coaching-calls/[!_]*.mdx', function (err, files) {
+  if (err) {
+    console.log('Error finding files: ' + err)
   } else {
-    let dataArr = [];
-    
+	 const rawData = fs.readFileSync('src/data/predefinedCoachingCallTags.json')
+    let coachingCallTagsData
+
+    try {
+      // Parse JSON data from file
+      coachingCallTagsData = JSON.parse(rawData)
+    } catch (e) {
+      // If error occurs during parsing, initialize as empty object.
+      console.error('Error parsing JSON file:', e)
+      coachingCallTagsData = {}
+    }
+    const dataArr = []
+
     // Loop through each file
-    for(let i=0; i<files.length; i++) {
-	 
-
+    for (let i = 0; i < files.length; i++) {
       // Read file as string
-      const fileContent = fs.readFileSync(files[i], 'utf8');
+      const fileContent = fs.readFileSync(files[i], 'utf8')
 
-      // Parse frontmatter from string 
-      const result = matter(fileContent);
+      // Parse frontmatter from string
+      const result = matter(fileContent)
 
-	  console.log(path.basename(files[i]))
-	  console.log(result.data.coach.toLowerCase())
-	  
-		let previewPath = `/img/coachingCalls/${result.data.coach_lower_case}-cf-coaching-call.png`;
-		console.log(previewPath.substring(1))
 
-	// Check if file exists, otherwise use generic image
-	if (!fs.existsSync('static' + previewPath)) {
-		console.log('TRUE')
-		  previewPath = '/img/coachingCalls/generic-cf-coaching-call.png';
-		}
+      let previewPath = `/img/coachingCalls/${result.data.coach_lower_case}-cf-coaching-call.png`
+
+      // Check if file exists, otherwise use generic image
+      if (!fs.existsSync('static' + previewPath)) {
+		  previewPath = '/img/coachingCalls/generic-cf-coaching-call.png'
+      }
 
       // Create a new object based on parsed frontmatter and filename.
-      let newDataObj ={
+      const newDataObj = {
         title: result.data.title,
         date: result.data.date,
         description: result.data.description,
         tags: result.data.tags || [],
-        
-        preview: previewPath,
-        
-        website:`/resources/recordings-coaching-calls/${path.basename(files[i], '.mdx')}`,
-        
-        source:`/resources/recordings-coaching-calls/${path.basename(files[i], '.mdx')}`
-       }
 
-       dataArr.push(newDataObj);
-      
+        preview: previewPath,
+
+        website: `/resources/recordings-coaching-calls/${path.basename(files[i], '.mdx')}`,
+
+        source: `/resources/recordings-coaching-calls/${path.basename(files[i], '.mdx')}`
+      }
+
+      dataArr.push(newDataObj)
+
+	   newDataObj.tags.forEach(tag => {
+        // Check if tag already exists in json data
+        if (!coachingCallTagsData.hasOwnProperty(tag)) {
+	  coachingCallTagsData[tag] = {
+            label: tag.charAt(0).toUpperCase() + tag.slice(1),
+            description: '',
+            color: '#000000'
+          }
+        };
+	   })
     }
 
     // Write JSON output to disk at /src/data/coachingCalls/
-    fs.writeJson('src/data/coachingCalls.json', dataArr);
-  }
+    fs.writeJson('src/data/coachingCalls.json', dataArr, { spaces: 2 })
+	// Convert your data into entries
+let entriesArray= Object.entries(coachingCallTagsData);
+
+// Sort them based on key
+//	  entriesArray.sort((a,b)=> a[1].label.localeCompare(b[1].label));
+	  entriesArray.sort((a,b)=> {
+   if(a[1].label.startsWith('Coach:') && !b[1].label.startsWith('Coach:')) return -1;
+   else if(!a[1].label.startsWith('Coach:') && b[1].label.startsWith('Coach:')) return 1;
+   else return a[0].localeCompare(b[0]);
 });
+
+
+// Convert sorted array back into object
+let sortedCoachingCallsTag={};
+for(let i=0;i<entriesArray.length;i++){
+   sortedCoachingCallsTag[entriesArray[i][0]]=entriesArray[i][1];
+}
+    fs.writeJson('src/data/coachingCallTags.json', sortedCoachingCallsTag, { spaces: 2 })
+  }
+})
--- src/utils/generate_coaching_call_pages.sh
+++ src/utils/generate_coaching_call_pages.sh
@@ -28,7 +28,7 @@ while IFS=$'\t' read -r field1 field2 field3 field4 field5 field6 field7 field8 
     # Use each line to create a new mdx file with the same content as the template
 
 
- sed "s/SIDEBAR_POSITION/$field2/g; s/DATE/$field1/g; s/TAGS/$FIELD_FAIL/g; s/TITLE/$field4 w\/ $field3 [$FORMATTED_DATE]/g; s/COACH/$field3/g; s/C_LOWER_CASE/$COACH_LOWER_CASE/g; s/DESCRIPTION/$field5/g; s/YOUTUBE_ID/$field6/g" src/data/coaching-call-recording-page-template.mdx > "resources/recordings-coaching-calls/${field1}-${COACH_LOWER_CASE}.mdx"
+ sed "s/SIDEBAR_POSITION/$field2/g; s/DATE/$field1/g; s/TAGS/$field7/g; s/TITLE/$field4 w\/ $field3 [$FORMATTED_DATE]/g; s/COACH/$field3/g; s/C_LOWER_CASE/$COACH_LOWER_CASE/g; s/DESCRIPTION/$field5/g; s/YOUTUBE_ID/$field6/g" src/data/coaching-call-recording-page-template.mdx > "resources/recordings-coaching-calls/${field1}-${COACH_LOWER_CASE}.mdx"
     
 
 
