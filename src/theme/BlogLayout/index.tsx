8a897ccd956bfb4540dcb38697d1223c8681f4e9
f <f@email>
2023-10-31 22:10:18 +0100

    moved offerings section on landing page and changed title size

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -5,28 +5,27 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import BlogSidebar from '@theme/BlogSidebar';
-import GcalEvents from '@site/src/components/Events';
-import { useLocation } from 'react-router-dom';
-import { HeroBanner,VideoContainer } from '@site/src/pages/_index.tsx';
-import styles from '@site/src/pages/styles.module.css';
-import Heading from '@theme/Heading';
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import BlogSidebar from '@theme/BlogSidebar'
+import GcalEvents from '@site/src/components/Events'
+import { useLocation } from 'react-router-dom'
+import { HeroBanner, VideoContainer } from '@site/src/pages/_index.tsx'
+import styles from '@site/src/pages/styles.module.css'
+import Heading from '@theme/Heading'
 import LiteYouTubeEmbed from 'react-lite-youtube-embed'
 
-import type {Props} from '@theme/BlogLayout';
+import type { Props } from '@theme/BlogLayout'
 
-export default function BlogLayout(props: Props): JSX.Element {
-  const {sidebar, toc, children, ...layoutProps} = props;
-  const hasSidebar = sidebar && sidebar.items.length > 0;
+export default function BlogLayout (props: Props): JSX.Element {
+  const { sidebar, toc, children, ...layoutProps } = props
+  const hasSidebar = sidebar && sidebar.items.length > 0
 
   // Get the current location
-  const location = useLocation();
+  const location = useLocation()
   // Check if the current path matches '/events'
-  const isEventsRoute = location.pathname === '/';
-
+  const isEventsRoute = location.pathname === '/'
 
   return (
     <Layout {...layoutProps}>
@@ -35,14 +34,7 @@ export default function BlogLayout(props: Props): JSX.Element {
               <>
 			<HeroBanner/>
 <div className={styles.section}>
-            <h1 id='events-overview'> Events Overview including Community-Created Events</h1>
-<p>This calendar is co-created by the community—find out how: <a href='how-to-engage/platforms-and-co-creation/co-create-on-hylo'>Co-Create on Hylo</a>. Learn how you can receive email invites for new events: <a href='how-to-engage/platforms-and-co-creation/receive-calendar-invites'>Receive Calendar Invites</a>.</p>
 
-<div className="gcal-events">
-            <GcalEvents />
-</div>
-</div>
-<div className={styles.section}>
             <h1 id='offerings'> Upcoming Offerings by CF Providers</h1>
 </div>
               </>
@@ -52,7 +44,7 @@ export default function BlogLayout(props: Props): JSX.Element {
           <main
             className={clsx('col', {
               'col--7': hasSidebar,
-              'col--9 col--offset-1': !hasSidebar,
+              'col--9 col--offset-1': !hasSidebar
             })}
             itemScope
             itemType="http://schema.org/Blog">
@@ -64,16 +56,24 @@ export default function BlogLayout(props: Props): JSX.Element {
             {isEventsRoute &&
               <>
 
-
               </>
             }
           </main>
         </div>
             {isEventsRoute &&
               <>
+
+<div className={styles.section}>
+            <h1 id='events-overview'> Events Overview including Community-Created Events</h1>
+<p>This calendar is co-created by the community—find out how: <a href='how-to-engage/platforms-and-co-creation/co-create-on-hylo'>Co-Create on Hylo</a>. Learn how you can receive email invites for new events: <a href='how-to-engage/platforms-and-co-creation/receive-calendar-invites'>Receive Calendar Invites</a>.</p>
+
+<div className="gcal-events">
+            <GcalEvents />
+</div>
+</div>
+<div className={styles.section}>
 <div className={styles.section}>
             <Heading as="h1"> Our Global Co-Creation Community on Hylo</Heading>
-
 
 <p>Below you see how our global community is distributed (updated Sep 2023). Explore the live map here: <a href='https://www.hylo.com/groups/convergent-facilitation/map?sortBy=updated&hide=discussion&hide=request&hide=offer&hide=resource&hide=project&hide=event&hide=group'>Hylo/Convergent Facilitation Group/Map</a>. If you are not part of our collaborative community space on Hylo find out how to join here: <a href='/how-to-engage/platforms-and-co-creation/co-create-on-hylo'>Co-Create on Hylo</a>.</p>
           <img
@@ -81,6 +81,7 @@ export default function BlogLayout(props: Props): JSX.Element {
             src="/img/hylo-community-map.webp"
           />
 
+</div>
 </div>
 <div className={styles.section}>
             <Heading as="h1"> Hear from Lisa on why bother Learning Convergent Facilitation</Heading>
@@ -99,5 +100,5 @@ export default function BlogLayout(props: Props): JSX.Element {
             }
       </div>
     </Layout>
-  );
+  )
 }
